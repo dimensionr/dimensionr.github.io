3a9b93b522dcfffce757b8f3c3f90d6c2a0853f4
Oromion <caznaranl@uni.pe>
2018-07-19 23:53:28 -0500

    Update6

--- gulpfile.js
+++ gulpfile.js
@@ -33,8 +33,18 @@ gulp.task('pug', function(){
 	.pipe(livereload());
 });
 
+gulp.task('pug:oromion', function(){
+  gulp.src(['./src/oromion/pug/post/**/*.pug'], ['./src/oromion/pug/index.pug'],)
+   .pipe(plumber())
+   .pipe(pug({
+     pretty: true
+   }))
+   .pipe(gulp.dest('./oromion/post/'))
+   .pipe(livereload());
+ });
+
 gulp.task('html', function () {
-  gulp.src(['./*.html'], ['./freider/post/*.html'], ['./oromion/post/*.html'], ['./debian-4ever/post/*.html'])
+  gulp.src(['./*.html'], ['./freider/post/*.html'], ['./oromion/post/*.html'], ['./debian-4ever/post/*.html'], './evn/post/*.html'])
     .pipe(gulp.dest('./'))
     .pipe(livereload());
 });
@@ -45,10 +55,14 @@ gulp.task('watch', function(){
   gulp.watch('./src/evn/stylus/*.styl', ['stylus']);
   gulp.watch(['./src/evn/pug/post/**/*.pug', './src/evn/pug/post/index.pug'], ['pug']);
   gulp.watch('./*.html', ['html']);
-  gulp.watch('./prueba/*.html', ['html']);
 });
 
-
+gulp.task('watch:oromion', function(){
+  livereload.listen();
+  gulp.watch('./src/oromion/stylus/*.styl', ['stylus:oromion']);
+  gulp.watch(['./src/oromion/pug/post/**/*.pug', './src/oromion/pug/post/index.pug'], ['pug:oromion']);
+  gulp.watch('./*.html', ['html']);
+});
 
 gulp.task('connect', function() {
   connect.server({
